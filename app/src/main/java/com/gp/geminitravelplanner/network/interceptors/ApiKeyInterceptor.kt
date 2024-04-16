af0f6f49b54f09814a0b411ff341e1d8878591cd
GrahamParvinkarimi <graham.parvinkarimi@gmail.com>
2024-04-15 23:52:01 -0400

    -Update APIKeyInterceptor to use query parameter instead of header

--- app/src/main/java/com/gp/geminitravelplanner/network/interceptors/ApiKeyInterceptor.kt
+++ app/src/main/java/com/gp/geminitravelplanner/network/interceptors/ApiKeyInterceptor.kt
@@ -3,6 +3,7 @@ package com.gp.geminitravelplanner.network.interceptors
 import com.gp.geminitravelplanner.BuildConfig
 import com.gp.geminitravelplanner.di.util.NetworkConstants
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 
 /*
@@ -11,9 +12,10 @@ import okhttp3.Response
 
 class ApiKeyInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request().newBuilder()
-            .addHeader(NetworkConstants.API_KEY_PARAM, BuildConfig.geoApifyAPIKey)
+        val url = chain.request().url.newBuilder()
+            .addQueryParameter(NetworkConstants.API_KEY_PARAM, BuildConfig.geoApifyAPIKey)
             .build()
+        val request: Request = chain.request().newBuilder().url(url).build()
         return chain.proceed(request)
     }
 }
