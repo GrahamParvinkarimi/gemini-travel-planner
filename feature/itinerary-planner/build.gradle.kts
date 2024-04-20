6ea71fc1c26ad991fc4aca8872171034496d40ab
GrahamParvinkarimi <graham.parvinkarimi@gmail.com>
2024-04-20 13:58:19 -0400

    -Add copy, close icons to bottom sheet

--- feature/itinerary-planner/build.gradle.kts
+++ feature/itinerary-planner/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.material.icons.extended)
     testImplementation(libs.junit)
 
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- feature/itinerary-planner/src/main/java/com/gp/itinerary_planner/ui/ItineraryPlannerScreen.kt
+++ feature/itinerary-planner/src/main/java/com/gp/itinerary_planner/ui/ItineraryPlannerScreen.kt
@@ -15,12 +15,16 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -33,13 +37,17 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ClipboardManager
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -69,73 +77,94 @@ fun ItineraryPlannerScreen(
 ) {
     var result by rememberSaveable { mutableStateOf("") }
 
+    if (uiState is UiState.Success) {
+        result = uiState.outputText
+    } else if (uiState is UiState.Loading) {
+        result = ""
+    }
+
     val location = remember { mutableStateOf("") }
     val days = remember { mutableStateOf("") }
 
     val sheetState = rememberModalBottomSheetState()
     val showBottomSheet = remember { mutableStateOf(true) }
+    val scope = rememberCoroutineScope()
+    val clipboardManager: ClipboardManager = LocalClipboardManager.current
 
     Column(
         modifier = Modifier
             .verticalScroll(rememberScrollState())
+            .fillMaxWidth()
     ) {
         Image(
-            modifier = Modifier
-                .height(204.dp)
-                .fillMaxWidth(),
             painter = painterResource(id = R.drawable.plane_travel_header_artwork),
             contentDescription = null
         )
 
         PlanYourTripCard(
+            uiState = uiState,
             location = location,
             days = days,
             showBottomSheet = showBottomSheet,
             sendPrompt = sendPrompt
         )
 
-        if (uiState is UiState.Loading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
-        } else {
-            var textColor = MaterialTheme.colorScheme.onSurface
-            if (uiState is UiState.Error) {
-                textColor = MaterialTheme.colorScheme.error
-                result = uiState.errorMessage
-            } else if (uiState is UiState.Success) {
-                textColor = MaterialTheme.colorScheme.onSurface
-                result = uiState.outputText
-            }
-            if (result.isNotEmpty() && showBottomSheet.value)
-                ModalBottomSheet(
-                    onDismissRequest = {
-                        showBottomSheet.value = false
-                    },
-                    sheetState = sheetState
+        if (result.isNotEmpty() && showBottomSheet.value) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet.value = false
+                },
+                sheetState = sheetState
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .verticalScroll(rememberScrollState())
+                        .fillMaxWidth()
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .align(Alignment.CenterHorizontally)
-                            .verticalScroll(rememberScrollState())
-                            .fillMaxWidth()
+                    Row(
+                        modifier = Modifier.align(Alignment.End)
                     ) {
-                        Text(
-                            text = "Elevated Card Title",
-                            color = MaterialTheme.colorScheme.primary
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(
-                            text = result, color = MaterialTheme.colorScheme.onSurface
-                        )
+                        IconButton(onClick = {
+                            clipboardManager.setText(AnnotatedString((result)))
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.ContentCopy,
+                                contentDescription = null,
+                            )
+                        }
+                        IconButton(onClick = {
+                            scope.launch { sheetState.hide() }.invokeOnCompletion {
+                                if (!sheetState.isVisible) {
+                                    showBottomSheet.value = false
+                                }
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Close,
+                                contentDescription = null,
+                            )
+                        }
                     }
+                    Text(
+                        text = "Itinerary for [dates]",
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = result, color = MaterialTheme.colorScheme.onSurface
+                    )
                 }
+            }
         }
-
     }
+
 }
 
 @Composable
 fun PlanYourTripCard(
+    uiState: UiState,
     location: MutableState<String>,
     days: MutableState<String>,
     showBottomSheet: MutableState<Boolean>,
@@ -182,20 +211,26 @@ fun PlanYourTripCard(
                     })
                 Spacer(modifier = Modifier.height(16.dp))
 
-                //Button to create itinerary
-                Row {
-                    Button(
-                        onClick = {
-                            showBottomSheet.value = true
-                            sendPrompt(
-                                location.value, days.value
-                            )
-                        }, enabled = ItineraryPlannerScreenUtils.isPlannerButtonEnabled(
-                            location = location.value, days = days.value
-                        ),
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text(text = stringResource(R.string.create_itinerary_button_text))
+                Row(
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                ) {
+                    if (uiState is UiState.Loading) {
+                        CircularProgressIndicator()
+                    } else {
+                        //Button to create itinerary
+                        Button(
+                            onClick = {
+                                showBottomSheet.value = true
+                                sendPrompt(
+                                    location.value, days.value
+                                )
+                            }, enabled = ItineraryPlannerScreenUtils.isPlannerButtonEnabled(
+                                location = location.value, days = days.value
+                            ),
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(text = stringResource(R.string.create_itinerary_button_text))
+                        }
                     }
                 }
             }
